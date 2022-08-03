bb2a6a1ffbd0508bb663713626a76c419b048b8e
Jackson Viana <jviana116@gmail.com>
2022-08-03 19:25:54 -0300

    feat: realizando uma requisição GET e criando o middleware para o requisito 6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 const { findTalker, findTalkerId, userToken, createTalker,
   createTalkerPart2, createTalkerPart3, addNewTalker,
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const PATH = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -30,6 +32,21 @@ app.post('/login', userToken, (_req, res) => {
 app.post('/talker', createTalker, createTalkerPart2, watchedAtValidation,
   createTalkerPart3, addNewTalker, (_req, res) => {
   res.status(HTTP_OK_STATUS);
+});
+
+app.put('/talker/:id', createTalker, createTalkerPart2, watchedAtValidation,
+createTalkerPart3, async (req, res) => {
+  const { id } = req.params;
+  const { age, name, talk } = req.body;
+
+  const talkers = await fs.readFile(PATH, 'utf8');
+  const talkers2 = JSON.parse(talkers);
+
+  const talkerIndex = talkers2.findIndex((r) => r.id === Number(id));
+  talkers2[talkerIndex] = { ...talkers2[talkerIndex], age, name, talk };
+  await fs.writeFile(PATH, JSON.stringify(talkers2));
+
+  return res.status(200).json(talkers2[talkerIndex]);
 });
 
 app.listen(PORT, () => {
--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -74,10 +74,10 @@ const watchedAtValidation = (req, res, next) => {
 
 const createTalkerPart3 = (req, res, next) => {
   const { talk: { rate } } = req.body;
-  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  if (rate < 1 || rate > 5) {
+  if (rate < 1 || rate > 5 || rate === 0) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
+  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   next();
 };
 
